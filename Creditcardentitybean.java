1a1018e7c42f430892269719c08cc80b84bb13a0
“jiku01” <priyankaunnikrishnan13@gmail.com>
2023-03-02 09:29:26 +0000

    RMS12345 - CREDITCARDPROJECT - MS -CREDIT DETAILS

---
+++ Creditcardentitybean.java
@@ -0,0 +1,21 @@
+
+//outer loop      
+for(int j = 1; j <= size; j++)  
+{  
+//calculates and prints the values for pattern  
+System.out.print(Math.max(Math.abs(i - n), Math.abs(j - n)) + 1 + " ");  
+}  
+System.out.println();  
+}  
+}  
+//driver code  
+public static void main(String args[])  
+{  
+Scanner sc = new Scanner(System.in);  
+System.out.print("Enter the value of n: ");  
+int n = sc.nextInt();  
+System.out.println();  
+//function calling  
+printPattern(n);  
+}  
+}  
